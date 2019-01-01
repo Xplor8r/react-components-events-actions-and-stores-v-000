d7a3791fc66b4f1ed3dc5e8670c04475eb1f7910
Xplor8r <mpieschel39@gmail.com>
2019-01-01 13:32:47 -0800

    add carousel to index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-const app = "I don't do much.";
+class Carousel extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        // We start out rendering the first slide. 0 denotes the index of the
+        // active item.
+        currentSlide: 0,
+      };
+    }
+    /**
+     * Handler function that transitions to the next slide in the carousel.
+     * This is the function that will be run once the user clicks the "next"
+     * button.
+     */
+    goNext = () => {
+      const previousSlide = this.state.currentSlide;
+      this.setState({ currentSlide: previousSlide + 1 });
+    }
+    /**
+     * Equivalent to `goNext`. Handler function that will be invoked when clicking
+     * the "back" button.
+     */
+    goBack = () => {
+      const previousSlide = this.state.currentSlide;
+      this.setState({ currentSlide: previousSlide - 1 });
+    }
+    render() {
+      // Magic goes here
+    }
+  }
